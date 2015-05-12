4e23afee7d01d0e03b2ad55fa90c9807cbc02398
Melvin Sembrano <melvinsembrano@gmail.com>
2015-05-12 13:11:30 +1000

    make sure karma will only execute browser tests

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'easy-date.js',
-      'test/**/*Spec.js'
+      'test/*Spec.js'
     ],
 
 
